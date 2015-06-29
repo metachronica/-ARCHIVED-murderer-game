944caeb818978c51483867d5e61904a6abfbeef7
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2015-06-29 23:30:51 +0300

    first structure

---
+++ front-end-src/scripts/main.ls
@@ -0,0 +1,8 @@
+/**
+ * @author Viacheslav Lotsmanov <lotsmanov89@gmail.com>
+ * @license GNU/AGPLv3
+ * @see {@link https://www.gnu.org/licenses/agpl-3.0.txt|License}
+ */
+
+console.log 321
+alert \hehe
---
+++ gulpfile.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var
+	c          = require('colors/safe'),
+	gulp       = require('gulp'),
+	del        = require('del'),
+	livescript = require('gulp-livescript'),
+	watch      = require('gulp-watch'),
+	plumber    = require('gulp-plumber'),
+	gutil      = require('gulp-util'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rename     = require('gulp-rename'),
+	path       = require('path'),
+	stylus     = require('gulp-stylus'),
+	uglify     = require('gulp-uglify'),
+	argv       = require('yargs').argv,
+	
+	plumberOpts = {
+		errorHanlder: function (err) {
+			console.error(c.red('Error:'), err.stack || err);
+			this.emit('end');
+		}
+	};
+
+function buildStart(logName) {
+	
+	if ( ! argv.buildLog) return gutil.noop();
+	
+	return rename(function (f) {
+		gutil.log(
+			c.blue(logName + ': building file'),
+			path.join(f.dirname, f.basename + f.extname)
+		);
+	});
+}
+
+function buildFinish(logName) {
+	
+	if ( ! argv.buildLog) return gutil.noop();
+	
+	return rename(function (f) {
+		gutil.log(
+			c.green(logName + ': file is builded'),
+			path.join(f.dirname, f.basename + f.extname)
+		);
+	});
+}
+
+
+// back-end livescript
+
+gulp.task('clean-server', function (cb) {
+	del(['server/build'], cb);
+});
+
+function serverTask(isWatcher, cb) {
+	
+	gulp.src('server/src/**/*.ls')
+		.pipe(isWatcher ? watch('server/src/**/*.ls') : gutil.noop())
+		.pipe(plumber(plumberOpts))
+		.pipe(buildStart('server'))
+		.pipe(sourcemaps.init())
+		.pipe(livescript({ bare: true }))
+		.pipe(sourcemaps.write())
+		.pipe(buildFinish('server'))
+		.pipe(gulp.dest('server/build'))
+		.on('finish', cb);
+}
+
+gulp.task('server', ['clean-server'], function (cb) { serverTask(false, cb); });
+gulp.task('server-watch', function (cb) { serverTask(true, cb); });
+
+
+// front-end styles
+
+gulp.task('clean-styles', function (cb) {
+	del(['static/css/build'], cb);
+});
+
+function stylesTask(isWatcher, cb) {
+	
+	gulp.src('front-end-src/styles/main.styl')
+		.pipe(isWatcher ? watch('front-end-src/styles/**/*.styl') : gutil.noop())
+		.pipe(buildStart('styles'))
+		.pipe(plumber(plumberOpts))
+		.pipe(sourcemaps.init())
+		.pipe(stylus({ compress: !!argv.min }))
+		.pipe(sourcemaps.write())
+		.pipe(buildFinish('styles'))
+		.pipe(gulp.dest('static/css/build'))
+		.on('finish', cb);
+}
+
+gulp.task('styles', ['clean-styles'], function (cb) { stylesTask(false, cb); });
+gulp.task('styles-watch', function (cb) { stylesTask(true, cb); });
+
+
+// front-end scripts
+
+gulp.task('clean-scripts', function (cb) {
+	del(['static/js/build'], cb);
+});
+
+function scriptsTask(isWatcher, cb) {
+	
+	gulp.src('front-end-src/scripts/**/*.ls')
+		.pipe(isWatcher ? watch('front-end-src/scripts/**/*.ls') : gutil.noop())
+		.pipe(buildStart('scripts'))
+		.pipe(plumber(plumberOpts))
+		.pipe(sourcemaps.init())
+		.pipe(livescript({ bare: true }))
+		.pipe(argv.min ? uglify({ preserveComments: 'some' }) : gutil.noop())
+		.pipe(sourcemaps.write())
+		.pipe(buildFinish('scripts'))
+		.pipe(gulp.dest('static/js/build'))
+		.on('finish', cb);
+}
+
+gulp.task('scripts', ['clean-scripts'], function (cb) { scriptsTask(false, cb); });
+gulp.task('scripts-watch', function (cb) { scriptsTask(true, cb); });
+
+
+// main tasks
+
+gulp.task('watch', ['server-watch', 'styles-watch', 'scripts-watch']);
+gulp.task('default', ['server', 'styles', 'scripts']);
